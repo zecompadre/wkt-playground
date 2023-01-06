9ff0b6a7ae9174fdd58ddc1ebb233da615e82a5e
ZeCompadre <zecompadre@gmail.com>
2023-01-06 23:34:07 +0000

    ongoing

--- main.js
+++ main.js
@@ -200,11 +200,18 @@ function init() {
 		plotWKT();
 	});
 
-	$(document).keyup(function (e) {
-		if (e.key === "Escape") { // escape key maps to keycode `27`
+	document.onkeydown = function (evt) {
+		evt = evt || window.event;
+		var isEscape = false;
+		if ("key" in evt) {
+			isEscape = (evt.key === "Escape" || evt.key === "Esc");
+		} else {
+			isEscape = (evt.keyCode === 27);
+		}
+		if (isEscape) {
 			$('#pan').trigger('click');
 		}
-	});
+	};
 
 	plotWKT();
 	changeUI();
