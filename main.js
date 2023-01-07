a73cfeae839695f8eab8fefacb7bf8a5a6aca682
ZeCompadre <zecompadre@gmail.com>
2023-01-07 20:28:40 +0000

    ongoing

--- main.js
+++ main.js
@@ -116,7 +116,7 @@ function init() {
 			case "modify":
 				interaction = new ol.interaction.Modify({
 					features: new ol.Collection(vector.getSource().getFeatures()),
-					deleteCondition: ol.events.condition.click
+					deleteCondition: ol.events.condition.dblclick
 				});
 
 				interaction.on('modifyend', updateWKY);
