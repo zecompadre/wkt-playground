0c43b7bdbfe11138851131f651811087f4b2e4a0
ZeCompadre <zecompadre@gmail.com>
2023-01-07 19:35:34 +0000

    ongoing

--- main.js
+++ main.js
@@ -198,10 +198,6 @@ function init() {
 		$(this).css({ borderColor: '', backgroundColor: '' });
 	});
 
-	$('#wktStringTextArea').on("change", function () {
-		plotWKT();
-	});
-
 	document.onkeydown = function (evt) {
 		evt = evt || window.event;
 		var isEscape = false;
