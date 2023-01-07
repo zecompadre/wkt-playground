9a9781672df1d66aea2ff057b2155adb8f6ca63f
ZeCompadre <zecompadre@gmail.com>
2023-01-07 23:39:53 +0000

    ongoing

--- main.js
+++ main.js
@@ -169,7 +169,7 @@ function init() {
 			attributions: ['Powered by Esri', 'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
 			attributionsCollapsible: false,
 			url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-			maxZoom: 20
+			maxZoom: 19
 		})
 	});
 
