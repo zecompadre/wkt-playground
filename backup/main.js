a25a3fdcc024f1f8cba0ab43ebedd482de37b95f
ZeCompadre <zecompadre@gmail.com>
2023-03-27 22:34:27 +0100

    ongoing

--- backup/main.js
+++ backup/main.js
@@ -217,6 +217,8 @@ function init() {
 		$(this).css({ borderColor: '', backgroundColor: '' });
 	});
 
+	$('#wktStringTextArea').on("paste", pasteWKT);
+
 	document.onkeydown = function (evt) {
 		evt = evt || window.event;
 		var isEscape = false;
