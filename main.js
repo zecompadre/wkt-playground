f51d9261c974ed7d7e0ebfcc578207fa17f7283b
ZeCompadre <zecompadre@gmail.com>
2023-03-30 22:45:15 +0100

    ongoing

--- main.js
+++ main.js
@@ -186,3 +186,30 @@ var app = (function () {
 	};
 
 }());
+
+
+function setupTabs() {
+	document.querySelectorAll('.tab-btn').forEach(button => {
+		button.addEventListener('click', () => {
+
+
+			const sidebar = button.parentElement;
+			const tabs = sidebar.parentElement;
+			const tabNumber = button.dataset.forTab;
+			const tabActivate = tabs.querySelector(`.tab-content[data-tab="${tabNumber}"]`)
+
+			sidebar.querySelectorAll('.tab-btn').forEach(button => {
+				button.classList.remove('tab-btn-active')
+			})
+			tabs.querySelectorAll('.tab-content').forEach(tab => {
+				tab.classList.remove('tab-content-active')
+			})
+			button.classList.add('tab-btn-active')
+			tabActivate.classList.add('tab-content-active')
+		})
+	})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	setupTabs();
+})
