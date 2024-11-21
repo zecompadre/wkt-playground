24be57b69376132e298f62c930718cbcdae4b59c
ZeCompadre <zecompadre@gmail.com>
2024-11-21 19:55:40 +0000

    Update script and stylesheet versions in index.html; increment version query parameters for main.js and main.css, and fix projection instantiation in main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var app = (function () {
 	var editColor = '#ec7063';
 	var snapColor = '#34495e';
 
-	var projection = new ol.Projection('EPSG:4326');
+	var projection = new ol.proj.Projection('EPSG:4326');
 
 	var latitude = 39.6945;
 	var longitude = -8.1234;
