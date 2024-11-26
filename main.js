639e5986df0524d86051e7996e5e7a71b945561b
ZeCompadre <zecompadre@gmail.com>
2024-11-26 19:44:45 +0000

    Update versioning in index.html and main.js; enhance feature selection logging in main.js

--- main.js
+++ main.js
@@ -548,7 +548,7 @@ var app = (function () {
 			var select = selectCtrl.getInteraction();
 			var active = select.getActive();
 			const features = select.getFeatures(); // Get the selected features collection
-			console.log("drawCtrl[change:active]", select, active, features);
+			console.log("drawCtrl[change:active]", select, active, typeof features);
 			if (!active && features.length > 0) {
 				var feature = features.item(0);
 				select.dispatchEvent({ type: 'select', selected: [], deselected: [feature] });
