6519a0b711bdc3821b32779f91666689e8b4106f
ZeCompadre <zecompadre@gmail.com>
2024-11-29 23:17:09 +0000

    Update versioning in index.html to v202411292316; change layer switcher button event from toggle to click in main.js

--- main.js
+++ main.js
@@ -749,7 +749,7 @@ var app = (function () {
 		map.addControl(layerSwitcherBtn);
 		mapControls.layerSwitcherBtn = layerSwitcherBtn;
 
-		layerSwitcherBtn.on('toggle', function (evt) {
+		layerSwitcherBtn.on('click', function (evt) {
 			console.log(evt)
 		});
 
