31c489914ff86b1dfd0f611d88d7a13baa7698a3
ZeCompadre <zecompadre@users.noreply.github.com>
2024-12-02 09:29:42 +0000

    Update main.js
    update attribution

--- main.js
+++ main.js
@@ -153,7 +153,7 @@ var app = (function () {
 		title: 'Satellite',
 		source: new ol.source.XYZ({
 			url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-			attributions: 'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer">ArcGIS</a>',
+			attributions: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
 		}),
 		visible: false,
 	});
